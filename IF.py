5f135a8cca1e99c65768a8280657470450e900e3
itai <itai.david1@mail.huji.ac.il>
2025-01-02 17:53:47 +0200

    construct interface functions

---
+++ IF.py
@@ -0,0 +1,12 @@
+
+def file_path2text(path):
+    ### extract the data from given path
+    return
+
+class Encdoer:
+    def encode(self, data):
+        return
+
+class Decoder:
+    def decode(self, data):
+        return
